72d6fb74e710ca79423a748f1b8379702714a50e
Justin Gallardo <justin.gallardo@gmail.com>
2016-02-08 15:22:53 -0800

    only use the contents of the first title tag on the page.

--- index.js
+++ index.js
@@ -149,7 +149,7 @@ Url.prototype.url = function(bot, to, from, msg, callback) {
           title = contentType;
         } else {
           parsed = cheerio.load(body);
-          title = parsed('title').text().replace(/\n/g, ' ').replace(/^\s+/, '');
+          title = parsed('title').eq(0).text().replace(/\n/g, ' ').replace(/^\s+/, '');
         }
       }
 
